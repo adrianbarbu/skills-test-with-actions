a577d5307f3a148d1cfdef370bab7b990922aee6
adrianbarbu <barbu_valentin88@yahoo.com>
2025-08-30 09:38:05 +0000

    test added

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -70,7 +70,6 @@ def test_get_nth_fibonacci_ten():
      assert result == 55
 
 def test_area_of_circle_negative_radius(self):
-     """Test with a negative radius to raise ValueError."""
      # Arrange
      radius = -1
 
